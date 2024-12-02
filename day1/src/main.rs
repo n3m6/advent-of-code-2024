eb049303b56de56f957ca4a16224dedd5a832ce8
Abdulla Faraz <abdulla.faraz@gmail.com>
2024-12-02 20:01:37 +0530

    day 1

--- day1/src/main.rs
+++ day1/src/main.rs
@@ -1,3 +1,39 @@
+use std::fs;
+
+fn read_lines(filename: &str) -> Vec<String> {
+    let mut result = Vec::new();
+
+    for line in fs::read_to_string(filename).unwrap().lines() {
+        result.push(line.to_string())
+    }
+
+    return result;
+}
+
 fn main() {
-    println!("Hello, world!");
+    println!("Reading file...");
+    let contents = read_lines("./data/day1.txt");
+    let mut arr1: Vec<i32> = Vec::new();
+    let mut arr2: Vec<i32> = Vec::new();
+
+    for line in contents.iter() {
+        let values: Vec<&str> = line.split_whitespace().collect();
+        println!("{} -> {}", values[0], values[1]);
+        arr1.push(values[0].parse::<i32>().unwrap());
+        arr2.push(values[1].parse::<i32>().unwrap());
+    }
+
+    arr1.sort();
+    arr2.sort();
+
+    let length = arr1.len();
+    println!("array length = {}", length);
+    let mut total = 0;
+
+    for i in 0..length {
+        let result = arr1[i] - arr2[i];
+        total += result.abs();
+    }
+
+    println!("total = {}", total);
 }
